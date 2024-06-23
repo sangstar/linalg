15e536c8ae8d8502c6dfb334c9e9abe5f90f124d
Sanger Steel <sangersteel@gmail.com>
2024-06-22 22:25:33 -0400

    docs: Update `README.md` on recent fmt changes

--- example.py
+++ example.py
@@ -3,20 +3,22 @@ import linalg
 a = linalg.Matrix([[1,3,8],[7,11,12],[1,2,3]])
 b = linalg.Matrix([[5,7,56],[12,8,14],[4,5,6]])
 
+# matrix a
 print(a)
 
-
+# row 1, column 2 of matrix a
 print(a[1,2])
 
-
+# a + b
 print(a + b)
 
-print("sub")
+# a - b
 print(a - b)
 
+# matrix a multiplied by matrix b
 c = a@b
 
 print(c)
 
-
+# determinant of matrix c
 print(c.det())
