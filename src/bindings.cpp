1b46ed5f1008566509a0410a196c473d5a9d3299
Sanger Steel <sangersteel@gmail.com>
2024-06-22 22:32:05 -0400

    docs: Fix messed up text in docs for `echelon_form`

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -23,8 +23,8 @@ NB_MODULE(linalg, m)
         .def("__getitem__", &Matrix<float>::operator[], "tuple"_a)
         .def("echelon_form", &Matrix<float>::echelon_form, "take_upper"_a = true, "take_lower"_a = false,
          "Get the row-echelon form of Matrix using LU decomposition, performed in-place. "
-         "`take_upper=True` retutake_lowerrns the upper triangular matrix U, which is Matrix in row-echelon "
-         "form, while `=True` returns the lower triangular matrix L. Note that this "
+         "`take_upper=True` returns the upper triangular matrix U, which is Matrix in row-echelon "
+         "form, while `take_lower=True` returns the lower triangular matrix L. Note that this "
          "means that take_upper = False and take_lower = False returns a matrix "
          "with both L and U encoded simultaneously in Matrix, which isn't meaningful as they're "
          "simply imputed together just to hold both in one matrix for memory efficiency.")
