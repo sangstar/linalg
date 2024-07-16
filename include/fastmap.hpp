554958e1e07dfb3f427ca0c2408234d110601f58
Sanger Steel <sangersteel@gmail.com>
2024-07-16 09:24:34 -0400

    chore: Remove comment

--- include/fastmap.hpp
+++ include/fastmap.hpp
@@ -42,40 +42,6 @@ int get_pivot_point(const Matrix<T> &data, int pivot_point, int num_points, Matr
     return argmax(pivot_distances);
 }
 
-/*
- The process for FastMap is as follows:
- We want to project row vectors making up the space into a lower
- dimension, which means we need to squash its projections in all but k
- dimensions. We do this as follows:
-
- 1. Find the longest line any two points can make in our data cloud.
- 2. Find the euclidean distance between every other point with that line
- cutting through the cloud
- 3. You'd now have a line through your data cloud, and all of your points
- have their own lines attaching perpendicular to that longest line, like straight hairs
- protruding from one long line. Those points exist in the reduced matrix, but they
- can only differ by a certain number of allowed dimensions. Take a look at this image:
- https://www.semanticscholar.org/paper/The-FastMap-Algorithm-for-Shortest-Path-Cohen-Uras/4b0d0fb7a8c5f91c051b10e506bd51d690735579/figure/0
-
- Notice that the plane below on the right, the reduced space only has room for 2 dimensions, which
- means that Oi and Oj can't differ by a third dimension anymore, and that distance is squashed.
- That top blue line represents the real distance between Oi and Oj, but in the reduced space that
- dimension doesn't exist. The bottom line happens to be the real distance with that third dimension
- squashed, as you can see when the points live on the 2D plane below. You can find that distance with
- trigonometry.
-
- The goal of the reduction is to essentially start with the hairy rod analogy from before, and align
- all the hairs at the same height along the rod -- reduce the dimensions by subtractings its component
- in unallowed directions so that its expression there is 0.
-
- A distance matrix is useful here to do all the calculations up front and reference it later
-
-
- So essentially:
-
- 1. Take
- */
-
 template<typename T>
 Matrix<T> get_distance_matrix(Matrix<T> &X, int n) {
     // Initialize the distances vector with the correct size
